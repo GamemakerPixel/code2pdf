78aa5b26d6fe43d3f0e28008c3011abb2da57662
Aidan Bensch <aidanbensch@gmail.com>
2026-03-06 02:19:56 -0500

    Draw some text

--- core/include/core/core.h
+++ core/include/core/core.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 namespace core {
 
 /// Creates a PDF with a single blank page.
--- core/src/code_pdf.cpp
+++ core/src/code_pdf.cpp
@@ -1,7 +1,10 @@
 #include "code_pdf.h"
 
+#include <AbstractContentContext.h>
+#include <PageContentContext.h>
 #include <PDFPage.h>
 #include <PDFWriter.h>
+#include <PDFUsedFont.h>
 
 #include <memory>
 #include <string>
@@ -13,6 +16,27 @@ void create_test_pdf(const std::string& filename) {
   writer.StartPDF(filename, ePDFVersion13);
   auto page = std::make_unique<PDFPage>();
   page->SetMediaBox(PDFRectangle(0, 0, 612, 792));
+	PageContentContext* context = writer.StartPageContentContext(page.get());
+
+	AbstractContentContext::GraphicOptions fill_options(
+		AbstractContentContext::eFill,
+		AbstractContentContext::eRGB,
+		0x000000
+	);
+	context->DrawCircle(306, 396, 36, fill_options);
+
+	PDFUsedFont* font = writer.GetFontForFile("regular.ttf");
+	AbstractContentContext::TextOptions text_options(
+		font,
+		14,
+		AbstractContentContext::eGray,
+		0
+	);
+	context->WriteText(72, 72, "Hello World!", text_options);
+	context->WriteText(72, 44, "Hello World, one line below!", text_options);
+	context->WriteText(72, 30, "And directly below!", text_options);
+
+	writer.EndPageContentContext(context);
   writer.WritePage(page.get());
   writer.EndPDF();
 }
